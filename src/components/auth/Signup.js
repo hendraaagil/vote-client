a69c9d822264685c42793541be9652619b1aa23b
hendraaagil <hendraaagil@gmail.com>
2020-11-22 21:37:11 +0700

    Change some styles & add signup component

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,16 +1,16 @@
-import { useState, useContext } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from '../../axios';
-import { AuthContext } from '../../contexts/AuthContext';
 
 import Welcome from '../layouts/Welcome';
 
-const Login = (props) => {
-  const { getUser } = useContext(AuthContext);
+const Signup = (props) => {
   const [state, setState] = useState({
+    fullName: '',
     username: '',
     password: '',
     errors: {
+      fullName: '',
       username: '',
       password: '',
     },
@@ -27,11 +27,14 @@ const Login = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
-      .post('/login', { username: state.username, password: state.password })
+      .post('/signup', {
+        fullName: state.fullName,
+        username: state.username,
+        password: state.password,
+      })
       .then((response) => {
         if (response.data.user) {
-          props.history.push('/vote');
-          getUser(response.data.user);
+          props.history.push('/login');
         }
       })
       .catch((error) => {
@@ -48,9 +51,25 @@ const Login = (props) => {
       <Welcome />
       <div className="flex flex-col justify-center items-center w-1/2">
         <h2 className="mb-5 font-semibold text-2xl text-center">
-          Silahkan login terlebih dahulu
+          Silahkan daftar terlebih dahulu
         </h2>
         <form className="flex flex-col">
+          <div className="mb-4 flex flex-col items-center">
+            <input
+              className="shadow appearance-none border rounded py-2 px-3 text-blueGray-800 leading-tight focus:outline-none focus:ring"
+              id="fullName"
+              name="fullName"
+              type="text"
+              placeholder="Nama Lengkap"
+              onChange={handleChange}
+              value={state.fullName}
+            />
+            {state.errors.fullName.length > 0 && (
+              <span className="mt-2 text-red-600 text-sm font-semibold text-center">
+                {state.errors.fullName}
+              </span>
+            )}
+          </div>
           <div className="mb-4 flex flex-col items-center">
             <input
               className="shadow appearance-none border rounded py-2 px-3 text-blueGray-800 leading-tight focus:outline-none focus:ring"
@@ -84,15 +103,15 @@ const Login = (props) => {
             )}
           </div>
           <button className="btn-dark" onClick={handleSubmit}>
-            Login
+            Daftar
           </button>
           <p className="mt-4 font-semibold text-center">
-            Belum punya akun?{' '}
+            Sudah punya akun?{' '}
             <Link
-              to="/signup"
+              to="/login"
               className="px-2 py-1 rounded-lg bg-blueGray-800 text-blueGray-300 border-blueGray-800 border-2 transition hover:bg-blueGray-300 hover:text-blueGray-800"
             >
-              Daftar
+              Login
             </Link>
           </p>
         </form>
@@ -101,4 +120,4 @@ const Login = (props) => {
   );
 };
 
-export default Login;
+export default Signup;
