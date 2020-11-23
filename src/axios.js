788dd3a2afc41b2d5eea55d3a4c8731da2650d93
hendraaagil <hendraaagil@gmail.com>
2020-11-23 22:29:08 +0700

    Change axios instance

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://vote-server-side.herokuapp.com/',
 });
 
 export default instance;
